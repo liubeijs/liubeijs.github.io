90657c5c21d29b4d3ee883b8727ead9941f90da6
Jiexin Li <lijiexin@cichang-inc.com>
2025-04-17 07:18:03 +0800

    add more links

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 const provinceDepartments = {
     "全国": {
         "中国政府网": "http://www.gov.cn",
+        "中国发展改革委": "https://www.ndrc.gov.cn",
+        "中国交通运输部": "https://www.mot.gov.cn",
+        "中国住房城乡建设部": "https://www.mohurd.gov.cn",
+        "中国水利部": "https://www.mwr.gov.cn",
+        "中国自然资源部": "https://www.mnr.gov.cn",
+        "中国生态环境部": "https://www.mee.gov.cn",
         "全国公共资源交易平台": "https://www.ggzy.gov.cn/",
         "全国公路监管平台": "https://hwdms.mot.gov.cn/BMWebSite/",
         "全国水利监管平台": "https://scjg.mwr.gov.cn/#/home"
@@ -12,6 +18,7 @@ const provinceDepartments = {
         "住建部门": "https://zjw.beijing.gov.cn",
         "水利部门": "https://swj.beijing.gov.cn",
         "自然资源": "https://ghzrzyw.beijing.gov.cn",
+        "生态环境": "http://sthjj.beijing.gov.cn",
         "公共资源": "https://ggzyfw.beijing.gov.cn"
     },
     "上海市": {
@@ -21,6 +28,7 @@ const provinceDepartments = {
         "住建部门": "https://zjw.sh.gov.cn",
         "水利部门": "https://swj.sh.gov.cn",
         "自然资源": "https://ghzyj.sh.gov.cn",
+        "生态环境": "https://sthj.sh.gov.cn",
         "公共资源": "https://www.shggzy.com/"
     },
     "天津市": {
@@ -30,6 +38,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjs.tj.gov.cn",
         "水利部门": "https://swj.tj.gov.cn",
         "自然资源": "https://ghhzrzy.tj.gov.cn",
+        "生态环境": "https://sthj.tj.gov.cn",
         "公共资源": "http://www.tjggzy.cn/"
     },
     "重庆市": {
@@ -39,6 +48,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjw.cq.gov.cn",
         "水利部门": "https://slj.cq.gov.cn",
         "自然资源": "http://ghzrzyj.cq.gov.cn",
+        "生态环境": "http://sthjj.cq.gov.cn",
         "公共资源": "https://www.cqggzy.com"
     },
     "河北省": {
@@ -48,6 +58,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjst.hebei.gov.cn",
         "水利部门": "http://slt.hebei.gov.cn",
         "自然资源": "https://zrzy.hebei.gov.cn",
+        "生态环境": "http://hbepb.hebei.gov.cn",
         "公共资源": "http://www.hebeieb.com/"
     },
     "山西省": {
@@ -57,6 +68,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.shanxi.gov.cn",
         "水利部门": "https://slt.shanxi.gov.cn",
         "自然资源": "http://zrzyt.shanxi.gov.cn",
+        "生态环境": "http://sthjt.shanxi.gov.cn",
         "公共资源": "http://prec.sxzwfw.gov.cn"
     },
     "内蒙古自治区": {
@@ -66,6 +78,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.nmg.gov.cn",
         "水利部门": "https://slt.nmg.gov.cn",
         "自然资源": "https://zrzy.nmg.gov.cn",
+        "生态环境": "http://sthjt.nmg.gov.cn",
         "公共资源": "http://ggzyjy.nmg.gov.cn"
     },
     "辽宁省": {
@@ -75,6 +88,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.ln.gov.cn",
         "水利部门": "https://slt.ln.gov.cn",
         "自然资源": "https://zrzy.ln.gov.cn",
+        "生态环境": "http://sthj.ln.gov.cn",
         "公共资源": "http://ggzy.ln.gov.cn"
     },
     "吉林省": {
@@ -84,6 +98,7 @@ const provinceDepartments = {
         "住建部门": "http://jst.jl.gov.cn",
         "水利部门": "http://slt.jl.gov.cn",
         "自然资源": "http://zrzy.jl.gov.cn",
+        "生态环境": "http://sthj.jl.gov.cn",
         "公共资源": "http://www.jl.gov.cn/ggzy"
     },
     "黑龙江省": {
@@ -93,6 +108,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjst.hlj.gov.cn",
         "水利部门": "https://slt.hlj.gov.cn",
         "自然资源": "http://zrzyt.hlj.gov.cn",
+        "生态环境": "https://sthj.hlj.gov.cn",
         "公共资源": "https://ggzyjyw.hlj.gov.cn/"
     },
     "江苏省": {
@@ -102,6 +118,7 @@ const provinceDepartments = {
         "住建部门": "https://jsszfhcxjst.jiangsu.gov.cn",
         "水利部门": "http://jswater.jiangsu.gov.cn",
         "自然资源": "https://zrzy.jiangsu.gov.cn",
+        "生态环境": "http://hbt.jiangsu.gov.cn",
         "公共资源": "http://jsggzy.jszwfw.gov.cn"
     },
     "浙江省": {
@@ -111,6 +128,7 @@ const provinceDepartments = {
         "住建部门": "https://jst.zj.gov.cn",
         "水利部门": "https://slt.zj.gov.cn",
         "自然资源": "https://zrzyt.zj.gov.cn",
+        "生态环境": "https://sthjt.zj.gov.cn",
         "公共资源": "https://ggzy.zj.gov.cn"
     },
     "安徽省": {
@@ -120,6 +138,7 @@ const provinceDepartments = {
         "住建部门": "http://dohurd.ah.gov.cn/index.html",
         "水利部门": "https://slt.ah.gov.cn",
         "自然资源": "https://zrzyt.ah.gov.cn",
+        "生态环境": "https://sthjt.ah.gov.cn",
         "公共资源": "http://ggzy.ah.gov.cn"
     },
     "福建省": {
@@ -129,6 +148,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.fujian.gov.cn",
         "水利部门": "https://slt.fujian.gov.cn",
         "自然资源": "https://zrzyt.fujian.gov.cn",
+        "生态环境": "https://sthjt.fujian.gov.cn",
         "公共资源": "https://ggzyfw.fujian.gov.cn"
     },
     "江西省": {
@@ -138,6 +158,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.jiangxi.gov.cn",
         "水利部门": "https://slt.jiangxi.gov.cn",
         "自然资源": "https://bnr.jiangxi.gov.cn",
+        "生态环境": "http://sthj.jiangxi.gov.cn",
         "公共资源": "https://www.jxsggzy.cn"
     },
     "山东省": {
@@ -147,6 +168,7 @@ const provinceDepartments = {
         "住建部门": "http://zjt.shandong.gov.cn",
         "水利部门": "http://wr.shandong.gov.cn",
         "自然资源": "http://dnr.shandong.gov.cn",
+        "生态环境": "http://sthj.shandong.gov.cn",
         "公共资源": "https://ggzyjy.shandong.gov.cn/"
     },
     "河南省": {
@@ -156,6 +178,7 @@ const provinceDepartments = {
         "住建部门": "https://hnjs.henan.gov.cn",
         "水利部门": "https://slt.henan.gov.cn",
         "自然资源": "https://dnr.henan.gov.cn",
+        "生态环境": "https://sthjt.henan.gov.cn",
         "公共资源": "https://hnsggzyjy.henan.gov.cn/"
     },
     "湖北省": {
@@ -165,6 +188,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.hubei.gov.cn",
         "水利部门": "https://slt.hubei.gov.cn",
         "自然资源": "https://zrzyt.hubei.gov.cn",
+        "生态环境": "http://sthjt.hubei.gov.cn",
         "公共资源": "http://www.hbggzy.cn"
     },
     "湖南省": {
@@ -174,6 +198,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.hunan.gov.cn",
         "水利部门": "https://slt.hunan.gov.cn",
         "自然资源": "http://zrzyt.hunan.gov.cn",
+        "生态环境": "http://sthjt.hunan.gov.cn",
         "公共资源": "https://ggzy.hunan.gov.cn",
         "政府采购": "http://www.ccgp-hunan.gov.cn/",
         "招投标监管": "http://bidding.fgw.hunan.gov.cn/",
@@ -185,6 +210,7 @@ const provinceDepartments = {
         "住建部门": "http://zfcxjst.gd.gov.cn",
         "水利部门": "http://slt.gd.gov.cn",
         "自然资源": "https://nr.gd.gov.cn",
+        "生态环境": "http://sthjt.gd.gov.cn",
         "公共资源": "https://ygp.gdzwfw.gov.cn/#/44/index"
     },
     "广西壮族自治区": {
@@ -194,6 +220,7 @@ const provinceDepartments = {
         "住建部门": "http://zjt.gxzf.gov.cn",
         "水利部门": "https://slt.gxzf.gov.cn",
         "自然资源": "https://dnr.gxzf.gov.cn",
+        "生态环境": "http://sthjt.gxzf.gov.cn",
         "公共资源": "http://gxggzy.gxzf.gov.cn/index.shtml"
     },
     "海南省": {
@@ -203,6 +230,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.hainan.gov.cn",
         "水利部门": "http://swt.hainan.gov.cn",
         "自然资源": "https://lr.hainan.gov.cn",
+        "生态环境": "http://sthjt.hainan.gov.cn",
         "公共资源": "https://ggzy.hainan.gov.cn/ggzyjy/",
         "建筑市场监管平台": "https://sys.hizj.net:8801/#/home",
     },
@@ -213,6 +241,7 @@ const provinceDepartments = {
         "住建部门": "http://jst.sc.gov.cn",
         "水利部门": "https://slt.sc.gov.cn",
         "自然资源": "http://dnr.sc.gov.cn",
+        "生态环境": "http://sthjt.sc.gov.cn",
         "公共资源": "https://ggzyjy.sc.gov.cn/jyxx/transactionInfo.html"
     },
     "贵州省": {
@@ -222,6 +251,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjst.guizhou.gov.cn",
         "水利部门": "https://mwr.guizhou.gov.cn",
         "自然资源": "https://zrzy.guizhou.gov.cn",
+        "生态环境": "http://sthjt.guizhou.gov.cn",
         "公共资源": "http://ggzy.guizhou.gov.cn"
     },
     "云南省": {
@@ -231,6 +261,7 @@ const provinceDepartments = {
         "住建部门": "https://zfcxjst.yn.gov.cn",
         "水利部门": "https://wcb.yn.gov.cn",
         "自然资源": "http://dnr.yn.gov.cn",
+        "生态环境": "http://sthjt.yn.gov.cn",
         "公共资源": "http://ggzy.yn.gov.cn"
     },
     "西藏自治区": {
@@ -240,6 +271,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.xizang.gov.cn",
         "水利部门": "http://slt.xizang.gov.cn",
         "自然资源": "http://zrzyt.xizang.gov.cn",
+        "生态环境": "http://sthjt.xizang.gov.cn",
         "公共资源": "http://ggzy.xizang.gov.cn"
     },
     "陕西省": {
@@ -249,6 +281,7 @@ const provinceDepartments = {
         "住建部门": "https://js.shaanxi.gov.cn",
         "水利部门": "https://slt.shaanxi.gov.cn",
         "自然资源": "https://zrzyt.shaanxi.gov.cn",
+        "生态环境": "http://sthjt.shaanxi.gov.cn",
         "公共资源": "http://www.sxggzyjy.cn"
     },
     "甘肃省": {
@@ -258,6 +291,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.gansu.gov.cn",
         "水利部门": "https://slt.gansu.gov.cn",
         "自然资源": "https://zrzy.gansu.gov.cn",
+        "生态环境": "https://sthjt.gansu.gov.cn",
         "公共资源": "http://ggzyjy.gansu.gov.cn"
     },
     "青海省": {
@@ -267,6 +301,7 @@ const provinceDepartments = {
         "住建部门": "http://zjt.qinghai.gov.cn",
         "水利部门": "http://slt.qinghai.gov.cn",
         "自然资源": "https://zrzyt.qinghai.gov.cn",
+        "生态环境": "https://sthj.qinghai.gov.cn",
         "公共资源": "http://www.qhggzyjy.gov.cn"
     },
     "宁夏回族自治区": {
@@ -276,6 +311,7 @@ const provinceDepartments = {
         "住建部门": "https://jst.nx.gov.cn",
         "水利部门": "https://slt.nx.gov.cn",
         "自然资源": "https://zrzyt.nx.gov.cn",
+        "生态环境": "http://sthjt.nx.gov.cn",
         "公共资源": "https://ggzyjy.fzggw.nx.gov.cn/"
     },
     "新疆维吾尔自治区": {
@@ -285,6 +321,7 @@ const provinceDepartments = {
         "住建部门": "https://zjt.xinjiang.gov.cn",
         "水利部门": "https://slt.xinjiang.gov.cn",
         "自然资源": "https://zrzyt.xinjiang.gov.cn",
+        "生态环境": "http://sthj.xinjiang.gov.cn",
         "公共资源": "http://ggzy.xinjiang.gov.cn"
     }
 };
